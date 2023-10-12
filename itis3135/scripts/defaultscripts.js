e34e3c06ff3b678f03d9512152251044f4bb04de
[jgaliger] <[jgaliger@uncc.edu]>
2023-10-11 20:00:12 -0400

    yes

--- itis3135/scripts/defaultscripts.js
+++ itis3135/scripts/defaultscripts.js
@@ -1,11 +1,12 @@
 
 function currentDate(){
-const currentDate = new Date();
-const day = date.getDate();
-const month = date.getMonth();
-const year = date.getFullYear();
-consoleLog(currentDate);
+var currentDate = new Date();
+var day = currentDate.getDate();
+var month = currentDate.getMonth();
+var year = currentDate.getFullYear();
+alert(currentDate);
 }
+
 function scriptTest(){
     alert("Scritp run")
 }
