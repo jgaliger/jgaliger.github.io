f437582c0100d28071b6ff1cb2bbb3dbee257caa
[jgaliger] <[jgaliger@uncc.edu]>
2023-11-20 11:43:05 -0500

    yes

--- itis3135/scripts/array.js
+++ itis3135/scripts/array.js
@@ -1,7 +1,7 @@
-document.addEventListener('DOMContentLoaded', function () {
- let salaries = [];
- let person =[];
- let counter = 0;
+let salaries = [];
+let person =[];
+let counter = 0;
+
  function addSalary(){
         const name = document.getElementById('name').value;
         const salary = document.getElementById('salary').value;
@@ -13,33 +13,31 @@ document.addEventListener('DOMContentLoaded', function () {
         person[counter] = name;
         counter++;
  }
- 
- function displayResults(){
-     let placeholder =0;
-    for(let i=0;i<person.length;i++){
-        placeholder += salaries[i];
-    }
-    placeholder /= salaries.length;
-    const average = placeholder;
-    const highest = Math.max(salaries);
 
-    const resultsDiv = document.getElementById('results');
-    resultsDiv.innerHTML = `<h2>Results</h2><p>Average Salary: ${average}</p><p>Highest Salary: ${highest}</p>`;
- }
+function displayResults(){
+    let placeholder =0;
+   for(let i=0;i<person.length;i++){
+       placeholder += salaries[i];
+   }
+   placeholder /= salaries.length;
+   const average = placeholder;
+   const highest = Math.max(salaries);
 
-function displaySalary(){
-    const tableBody = document.getElementById('salaryTable');
-            tableBody.innerHTML = '';
-
-    for(let i=0; i<person.length;i++){
-        const row =tableBody.insertRow();
-        const firstRow = row.insertCell(0);
-        const secondRow = row.insertCell(1);
-
-        firstRow.textContent = person[i];
-        secondRow.textContent = salaries[i];
-    }
+   const resultsDiv = document.getElementById('results');
+   resultsDiv.innerHTML = `<h2>Results</h2><p>Average Salary: ${average}</p><p>Highest Salary: ${highest}</p>`;
 }
 
 
-});
+function displaySalary(){
+   const tableBody = document.getElementById('salaryTable');
+           tableBody.innerHTML = '';
+
+   for(let i=0; i<person.length;i++){
+       const row =tableBody.insertRow();
+       const firstRow = row.insertCell(0);
+       const secondRow = row.insertCell(1);
+
+       firstRow.textContent = person[i];
+       secondRow.textContent = salaries[i];
+   }
+}
