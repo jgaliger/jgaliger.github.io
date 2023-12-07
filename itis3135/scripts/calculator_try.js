3c74c594b28ae63643c56b7c3177896169c63d7e
[jgaliger] <[jgaliger@uncc.edu]>
2023-12-07 17:51:14 -0500

    yas

--- itis3135/scripts/calculator_try.js
+++ itis3135/scripts/calculator_try.js
@@ -1,46 +1,31 @@
-const calculator = document.querySelector(calculator)
-const keys = calculator.querySelector(calculator__keys)
-const display = document.querySelector('.calculator__display')
 const calculator = document.querySelector('.calculator')
-const previousKeyType = calculator.dataset.previousKeyType
-
+const keys = calculator.querySelector('calculator__keys')
 
 keys.addEventListener('click', e => {
-  if (e.target.matches('button')) {
+ if (e.target.matches(button)) {
     const key = e.target
     const action = key.dataset.action
-    const keyContent = key.textContent
-    const displayedNum = display.textContent
-  }
-  if (!action) {
-    if (displayedNum === '0') {
-      display.textContent = keyContent
-    } else {
-      display.textContent = displayedNum + keyContent
-    }
-  }
-  if (action === 'decimal') {
-    display.textContent = displayedNum + '.'
-  }
-  if (
-    action === 'add' ||
-    action === 'subtract' ||
-    action === 'multiply' ||
-    action === 'divide'
-  ) {
-    key.classList.add('is-depressed')
-  }
-  if (e.target.matches('button')) {
-    const key = e.target
-    Array.from(key.parentNode.children)
-      .forEach(k => k.classList.remove('is-depressed'))
-  }
-  if (!action) {
-    if (displayedNum === '0' || previousKeyType === 'operator') {
-      display.textContent = keyContent
-    } else {
-      display.textContent = displayedNum + keyContent
-    }
-  }
+    if (!action) {
+        console.log('number key!')
+      }
+      if (
+        action === 'add' ||
+        action === 'subtract' ||
+        action === 'multiply' ||
+        action === 'divide'
+      ) {
+        console.log('operator key!')
+      }
+      if (action === 'decimal') {
+        console.log('decimal key!')
+      }
+      
+      if (action === 'clear') {
+        console.log('clear key!')
+      }
+      
+      if (action === 'calculate') {
+        console.log('equal key!')
+      }
+ }
 })
-
--- itis3135/scripts/survey.js
+++ itis3135/scripts/survey.js
@@ -4,6 +4,45 @@ function reset() {
 function required(event) {
     event.preventDefault(); 
 }
+function submit() {
+    const name = document.getElementById('name').value;
+    const mascot = document.getElementById('mascot').value;
+    const image = document.getElementById('image').value
+    const imageCaption = document.getElementById('caption').value;
+    const personal = document.getElementById('personal').value
+    const academic = document.getElementById('academic').value
+    const background = document.getElementById('background').value
+    const platform = document.getElementById('platform').value
+    const courses = document.getElementById('p[id="courses[]"]')
+    const funny = document.getElementById('funny').value
+    const anything = document.getElementById('anything').value
+
+    const resultsDiv = document.getElementById('result');
+    resultsDiv.innerHTML = 
+   
+    `<h2>Name: ${name}</h2>
+    <h2>Mascot: ${mascot}</h2>
+    <h2>Image: <img src="${image}" alt="Image"></img></h2>
+    <figcaption>${imageCaption}</figcaption>
+    <p>Personal Background: ${personal}</p>
+    <p>Academic Background: ${academic}</p>
+    <p>Background in Web Development: ${background}</p>
+    <h4>Platform: ${platform}</h4>
+    <p>Funny thing about yourself: ${funny}</p>
+    <p>Something else about yourself: ${anything}</p>`;
+   
+    
+    const coursesList = document.getElementById('courses[]');
+    const courseInputs = coursesList.getElementsByTagName('input');
+
+    for (let i = 0; i < courseInputs.length; i++) {
+        resultsDiv.innerHTML += `<p>Course ${i + 1}: ${courseInputs[i].value}</p>`;
+    }
+
+    const contentDiv = document.getElementById('data');
+    contentDiv.innerHTML = "";
+    contentDiv.appendChild(submittedContent);
+}
 
 let courseArr = [];
 
@@ -26,45 +65,16 @@ function addCourse() {
         function deleteCourse(){
             courseList.removeChild(courseInput);
             courseList.removeChild(button);
+            courseList.removeChild(br);
         }
    }
+
 
     addEventListener('DOMContentLoaded',function(event){
         const introForm = document.getElementById('introForm');
         const introContent = document.getElementById('data');
    
 
-        introForm.addEventListener('submit', function (event) {
-          
-        const name = document.getElementById('name').value;
-        const mascot = document.getElementById('mascot').value;
-        const image = document.getElementById('image').value
-        const imageCaption = document.getElementById('caption').value;
-        const personal = document.getElementById('personal').value
-        const academic = document.getElementById('academic').value
-        const background = document.getElementById('background').value
-        const platform = document.getElementById('platform').value
-        const courses = document.getElementById('input[name="courses[]"]')
-        const funny = document.getElementById('funny').value
-        const anything = document.getElementById('anything').value
-    
-        const newForm = 
-        
-        `<h2>Name: ${name}</h2>
-        <h2>Mascot: ${mascot}</h2>
-        <h2>Image: <img src="${image}" alt="Image"></img></h2>
-        <figcaption>${imageCaption}</figcaption>
-        <p>Personal Background: ${personal}</p>
-        <p>Academic Background: ${academic}</p>
-        <p>Background in Web Development: ${background}</p>
-        <h4>Platform: ${platform}</h4>
-        <p>Courses: ${Array.from(courses).map(course => course.value).join(', ')}</p>
-        <p>Funny thing about yourself: ${funny}</p>
-        <p>Something else about yourself: ${anything}</p>`
-
-        const resultsDiv = document.getElementById('result');
-        resultsDiv.innerHTML = newForm;
-      });
     });
 
 
