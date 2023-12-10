fce7acb8984724c1653755642ba60b3eb2931689
[jgaliger] <[jgaliger@uncc.edu]>
2023-12-10 18:11:00 -0500

    e

--- itis3135/client/client.js
+++ itis3135/client/client.js
@@ -1,7 +1,17 @@
 let rentArr = [];
 let adviceArr = [];
 
+function reset() {
+    document.getElementById("introForm").reset();
+}
+function required(event) {
+    event.preventDefault(); 
+}
+
 function submit(){
+    addEventListener('DOMContentLoaded',function(event){
+    const introForm = document.getElementById('introForm');
+    const introContent = document.getElementById('data');
     const name = document.getElementById('name')
     const email = document.getElementById('email')
     const phone = document.getElementById('phone')
@@ -17,5 +27,6 @@ function submit(){
         rentArr.push(person)
     }
     const result = document.getElementById('resultDiv')
-    resultDiv.innerHTML = `<h3>Welcome, ${name}! We will attempt to get back to you as soon as possible!</h3>`
+    resultDiv.innerHTML = `<h3>Welcome, ${name}! We will attempt to get back to you as soon as possible!</h3>`;
+});
 }
