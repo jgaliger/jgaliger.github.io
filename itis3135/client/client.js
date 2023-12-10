c6d63bcefb92d20e2cad7842e6473f6d2ef1a795
[jgaliger] <[jgaliger@uncc.edu]>
2023-12-10 18:29:51 -0500

    f

--- itis3135/client/client.js
+++ itis3135/client/client.js
@@ -8,34 +8,29 @@ function required(event) {
     event.preventDefault(); 
 }
 
-function submit(){
-   
-    const introForm = document.getElementById('introForm').value;
-    const introContent = document.getElementById('data').value;
+function submit(event) {
+
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
     const phone = document.getElementById('number').value;
-    const address = document.getElementById('address').value
-    const area = document.getElementById('area').value
+    const address = document.getElementById('address').value;
+    const area = document.getElementById('area').value;
+    const check = document.getElementById('check').checked;
 
-    const value = document.getElementById('check').value
     const person = `Name: ${name} Email: ${email} Phone Number: ${phone} Address: ${address} Area: ${area}`;
-    const check = document.getElementById('check').checked
-    if(check){
-        adviceArr.push(person);
-    }else{
-        rentArr.push(person)
-    }
-    const result = document.getElementById('resultDiv')
-    resultDiv.innerHTML = `<h3>Welcome, ${name}! We will attempt to get back to you as soon as possible!</h3>`;
 
+    if (check) {
+        adviceArr.push(person);
+    } else {
+        rentArr.push(person);
+    }
+
+    const resultDiv = document.querySelector('.resultDiv');
+    resultDiv.innerHTML = `<h3>Welcome, ${name}! We will attempt to get back to you as soon as possible!</h3>`;
 }
 
 addEventListener('DOMContentLoaded', function (event) {
 
     const data = document.getElementById('data');
-    data.addEventListener('click', function () {
-        submit();
-
-    });
+    const introForm = document.getElementById('introForm')
 });
