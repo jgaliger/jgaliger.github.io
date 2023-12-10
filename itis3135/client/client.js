6aa15a9d31893051b4823e5033336debd5adf6b4
[jgaliger] <[jgaliger@uncc.edu]>
2023-12-10 18:24:18 -0500

    yes

--- itis3135/client/client.js
+++ itis3135/client/client.js
@@ -9,24 +9,33 @@ function required(event) {
 }
 
 function submit(){
-    addEventListener('DOMContentLoaded',function(event){
-    const introForm = document.getElementById('introForm');
-    const introContent = document.getElementById('data');
-    const name = document.getElementById('name')
-    const email = document.getElementById('email')
-    const phone = document.getElementById('phone')
-    const address = document.getElementById('address')
-    const area = document.getElementById('area')
+   
+    const introForm = document.getElementById('introForm').value;
+    const introContent = document.getElementById('data').value;
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const phone = document.getElementById('number').value;
+    const address = document.getElementById('address').value
+    const area = document.getElementById('area').value
 
-    const value = document.getElementById('check')
-    const person;
-    person.textContent= "Name: " + name + " Email: " + email + " Phone Number: " + phone + " Address: " + address + " Area: " + area;
-    if(document.getElementById("check").checked===true){
+    const value = document.getElementById('check').value
+    const person = `Name: ${name} Email: ${email} Phone Number: ${phone} Address: ${address} Area: ${area}`;
+    const check = document.getElementById('check').checked
+    if(check){
         adviceArr.push(person);
     }else{
         rentArr.push(person)
     }
     const result = document.getElementById('resultDiv')
     resultDiv.innerHTML = `<h3>Welcome, ${name}! We will attempt to get back to you as soon as possible!</h3>`;
-});
+
 }
+
+addEventListener('DOMContentLoaded', function (event) {
+
+    const data = document.getElementById('data');
+    data.addEventListener('click', function () {
+        submit();
+
+    });
+});
